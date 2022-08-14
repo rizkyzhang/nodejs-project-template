cc6055275b05205e2086b0cf1c443bb4b974885c
rizkyzhang <rizkyzhangdev@gmail.com>
2022-08-14 14:10:04 +0700

    feat: update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import cors from "cors"
 import helmet from "helmet"
 import morgan from "morgan"
 
+import root from "@/src/routes/root"
+
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -12,14 +14,10 @@ app.use(helmet())
 app.use(morgan("short"))
 
 app.get("/", (req, res) => {
-  res.redirect("/api")
+  res.redirect("/api/v1")
 })
 
-app.get("/api", (req, res) => {
-  res.json({
-    message: "Welcome to starter API",
-  })
-})
+app.use("/api/v1", root)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
